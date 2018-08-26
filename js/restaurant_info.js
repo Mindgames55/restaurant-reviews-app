bd0935e831c69ecfd4ec477a7fa8acb2ed01a6d3
Mayguen Ojeda <mayguen.ojeda88@gmail.com>
2018-08-26 19:22:35 -0400

    PWA manifest

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -1,16 +1,3 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/service-worker.js')
-    .then(function(registration) {
-      console.log('Registration successful, scope is:', registration.scope);
-    })
-    .catch(function(error) {
-      console.log('Service worker registration failed, error:', error);
-    });
-  });
-}
-
-
 let restaurant;
 var newMap;
 
