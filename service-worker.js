82d8c058f44e24793c08c79d929b7a2d489c20d6
Mayguen Ojeda <mayguen.ojeda88@gmail.com>
2018-08-27 10:15:18 -0400

    fix filter screen

--- service-worker.js
+++ service-worker.js
@@ -9,7 +9,7 @@ var filesToCache = [
   'js/dbhelper.js'
 ];//
 
-var staticCacheName = 'pages-cache-v12';
+var staticCacheName = 'pages-cache-v13';
 
 self.addEventListener('install', function(event) {
   console.log('Attempting to install service worker and cache static assets');
