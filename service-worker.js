31eaf12fb7906ef83e080bbdda4af750de9fb383
Mayguen Ojeda <mayguen.ojeda88@gmail.com>
2018-08-27 10:52:57 -0400

    sw new v

--- service-worker.js
+++ service-worker.js
@@ -9,7 +9,7 @@ var filesToCache = [
   'js/dbhelper.js'
 ];//
 
-var staticCacheName = 'pages-cache-v15';
+var staticCacheName = 'pages-cache-v16';
 
 self.addEventListener('install', function(event) {
   console.log('Attempting to install service worker and cache static assets');
