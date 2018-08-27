d7fe031f532436beb258491506807f254f7a396a
Mayguen Ojeda <mayguen.ojeda88@gmail.com>
2018-08-27 09:24:24 -0400

    A2HS prompt

--- js/main.js
+++ js/main.js
@@ -288,6 +288,27 @@ const view = {
 //INITIALIZATION************************
 document.addEventListener('DOMContentLoaded', (event) => {
   view.homeScreen.init();
+
+  let deferredPrompt;
+
+  window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+
+    deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice
+    .then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+    });
+  });
 });
 
 const octopus = {
