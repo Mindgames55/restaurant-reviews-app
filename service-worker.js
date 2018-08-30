f4fcc3d2fc8387e83ceb17124e73e27ed078f71d
Mindgames55 <37992848+Mindgames55@users.noreply.github.com>
2018-08-30 09:20:53 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -9,7 +9,7 @@ var filesToCache = [
   'js/dbhelper.js'
 ];//
 
-var staticCacheName = 'pages-cache-v12';
+var staticCacheName = 'pages-cache-v1';
 
 self.addEventListener('install', function(event) {
   console.log('Attempting to install service worker and cache static assets');
